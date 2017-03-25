21c83e1dbb05ebcba8e99af844ae7b162bef7752
lvalido <lvalido@mailexternos.com.ar>
2017-03-24 22:43:25 -0300

    [Mervalito.API] Initialize Project

---
+++ Mervalito.API/Controllers/TitleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http;
+
+namespace Mervalito.API.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="System.Web.Http.ApiController" />
+    [RoutePrefix("")]
+    public class TitleController : ApiController
+    {
+        /// <summary>
+        /// Lists this instance.
+        /// </summary>
+        /// <returns></returns>
+        [Route("titles")]
+        [HttpGet]
+        public string List()
+        {
+            return "list";
+        }
+        /// <summary>
+        /// Updates the specified title identifier.
+        /// </summary>
+        /// <param name="titleId">The title identifier.</param>
+        /// <returns></returns>
+        [Route("titles/{titleId:int}")]
+        [HttpPut]
+        public string Update(int titleId)
+        {
+            return "update";
+        }
+    }
+}
